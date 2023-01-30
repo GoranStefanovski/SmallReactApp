1fcd8f10b0a846b92f97589b15e25d45b2221093
Goran Stefanovski <tevidma@gmail.com>
2023-01-30 15:12:59 +0100

    recording tab fix

--- src/_components/RecordingTab.tsx
+++ src/_components/RecordingTab.tsx
@@ -8,7 +8,7 @@ import { isMessageTrue } from '../../src/features/message/messageSlice';
 import { isCountingPaused } from '../../src/features/counting/countingSlice';
 import MyStopwatch from './MyStopwatch'
 import { useEffect, useRef } from 'react'
-
+import Draggable from 'react-draggable'; 
 
 function RecordingTab() {
     const isRecording = useSelector((state: any) => state.recording.value)
@@ -29,73 +29,15 @@ function RecordingTab() {
             dispatch(isCountingPaused());
         }
     }
-
-    const containerRef = useRef<HTMLDivElement>(null);
-    const boxRef = useRef<HTMLDivElement>(null);
-  
-    const isClicked = useRef<boolean>(false);
-  
-    const coords = useRef<{
-      startX: number,
-      startY: number,
-      lastX: number,
-      lastY: number
-    }>({
-      startX: 150,
-      startY: 150,
-      lastX: 150,
-      lastY: 150
-    })
-  
-    useEffect(() => {
-      if (!boxRef.current || !containerRef.current) return;
-  
-      const box = boxRef.current;
-      const container = containerRef.current;
-  
-  
-      const onMouseDown = (e: MouseEvent) => {
-        isClicked.current = true;
-        coords.current.startX = e.clientX;
-        coords.current.startY = e.clientY;
-      }
-  
-      const onMouseUp = (e: MouseEvent) => {
-        isClicked.current = false;
-        coords.current.lastX = box.offsetLeft;
-        coords.current.lastY = box.offsetTop;
-      }
-  
-      const onMouseMove = (e: MouseEvent) => {
-        if (!isClicked.current) return;
-  
-        const nextX = e.clientX - coords.current.startX + coords.current.lastX;
-        const nextY = e.clientY - coords.current.startY + coords.current.lastY;
-  
-        box.style.top = `${nextY}px`;
-        box.style.left = `${nextX}px`;
-      }
-  
-      box.addEventListener('mousedown', onMouseDown);
-      box.addEventListener('mouseup', onMouseUp);
-      container.addEventListener('mousemove', onMouseMove);
-      container.addEventListener('mouseleave', onMouseUp);
-  
-      const cleanup = () => {
-        box.removeEventListener('mousedown', onMouseDown);
-        box.removeEventListener('mouseup', onMouseUp);
-        container.removeEventListener('mousemove', onMouseMove);
-        container.removeEventListener('mouseleave', onMouseUp);
-      }
-  
-      return cleanup;
-    }, [])
     
   return (
-    <div ref={containerRef} className="container">
-        <div ref={boxRef} style={{display: isRecording ? 'flex' : 'none'}} className='recording'>
+    <Draggable
+    handle=".ctr"
+    defaultPosition={{x: 0, y: 0}}
+    scale={1}>
+        <div  style={{display: isRecording ? 'flex' : 'none'}} className='recording'>
             <div className='recording_inner'>
-                <span className='recording-time recording_inner-rotate'>
+                <span className='recording-time recording_inner-rotate ctr'>
                     <MyStopwatch runing={false}></MyStopwatch>
                 </span>
                 <span onClick={pauseRecording} className='control recording-pause recording_inner-rotate'>
@@ -109,7 +51,7 @@ function RecordingTab() {
                 </span>
             </div>
         </div>
-    </div>
+    </Draggable>
   )
 }
 
